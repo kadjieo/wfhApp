0fb91a380cc70279e315411daf5b84278088ca24
Dara Khadjehnouri <dara.khadjehnouri@hiq.se>
2021-05-20 15:20:38 +0200

    Hackathon0.3

--- app/src/main/java/com/ericsson/workfromhomeconcept/DisplayPage.java
+++ app/src/main/java/com/ericsson/workfromhomeconcept/DisplayPage.java
@@ -2,7 +2,9 @@ package com.ericsson.workfromhomeconcept;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayPage extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class DisplayPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_page);
+        Intent intent = this.getIntent();
+        Bundle bundle = intent.getExtras();
+        String activity = bundle.getString("activity");
+
+        TextView tv = (TextView) findViewById(R.id.activity_display_text);
+        tv.setText(activity);
     }
 }
--- app/src/main/java/com/ericsson/workfromhomeconcept/MainActivity.java
+++ app/src/main/java/com/ericsson/workfromhomeconcept/MainActivity.java
@@ -1,18 +1,32 @@
 package com.ericsson.workfromhomeconcept;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    List<String> activities;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        activities = Arrays.asList("Walk for 20 minutes!", "Run for 30 minutes!", "Do yoga for 30 minutes!", "Play padel!",
+                "Play badminton!", "Go for a swim!", "Play some tennis!", "Listen to music!", "Stretch your back and legs!",
+                "Do Cardio for 40 minutes!");
         setContentView(R.layout.activity_main);
 
         Button activityGenerator = (Button) findViewById(R.id.activities);
@@ -33,11 +47,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTick(long millisUntilFinished) {
                 long millisToSeconds = millisUntilFinished / 1000;
-                tv.setText(String.valueOf(millisToSeconds));
+                tv.setText(String.valueOf(millisToSeconds + 1));
             }
 
             @Override
             public void onFinish() {
+                String activity = getRandomActivityFromList(activities);
+                intent.putExtra("activity",activity);
                 startActivity(intent);
             }
         }.start();
@@ -50,8 +66,15 @@ public class MainActivity extends AppCompatActivity {
 
     private void goToActivityGenerator(Class c) {
         Intent intent = new Intent(this, c);
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("activities", (Serializable) activities);
+        intent.putExtras(bundle);
         startActivity(intent);
     }
-
+    private String getRandomActivityFromList(final List<String> activities) {
+        int size = activities.size();
+        int randomIndex = new Random().nextInt(size);
+        return activities.get(randomIndex);
+    }
 
 }
